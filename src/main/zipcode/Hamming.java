35a8b6e95a80d8c2fe5b0149a6d9226f22a1ae08
Abiel Figueroa <Abe@zipcodes-MacBook-Pro-2.local>
2021-04-08 19:19:16 -0400

    DONE

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,26 @@
-package zipcode;
+package main.zipcode;
 
 public class Hamming {
+    String first;
+    String second;
     public Hamming(String s, String s1) {
+        first=s;
+        second=s1;
+        if (first.length() != second.length()) throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
     }
 
-    public int getHammingDistance() {
-        return -1;
-    }
-}
+    public int getHammingDistance(){
+        int variance = 0;
+        /*int smaller;
+        if(first.length()>second.length()){
+        variance=first.length()-second.length();
+        smaller=second.length();
+        }else{variance=second.length()-first.length();
+            smaller=first.length();}*/
+
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
+                variance++;
+            }
+        }return variance;
+    }}
